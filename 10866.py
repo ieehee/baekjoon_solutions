0509a17e65f2ef67ff28ca26bdc673858a2d1546
LyChee <60761820+ieehee@users.noreply.github.com>
2021-10-06 22:51:03 +0900

    solve 10866.py

---
+++ 10866.py
@@ -0,0 +1,37 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+N = int(input())
+
+d = deque([])
+for _ in range(N):
+    comm = input().rstrip().split()
+    if comm[0] == "push_back":
+        d.append(comm[1])
+
+    if comm[0] == "push_front":
+        d.appendleft(comm[1])
+
+    if comm[0] == "pop_front":
+        if len(d) != 0:
+            print(d.popleft())
+        else:
+            print(-1)
+
+    if comm[0] == "pop_back":
+        if len(d) != 0:
+            print(d.pop())
+        else:
+            print(-1)
+
+    if comm[0] == "front":
+        print(d[0])
+
+    if comm[0] == "back":
+        print(d[-1])
+
+    if comm[0] == "empty":
+        print(int(len(d) == 0))
+
+    if comm[0] == "size":
+        print(len(d))
